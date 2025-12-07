d365d2db94f6736288945e195e63f3f17b156760
Markus Becker <mtib.becker@gmail.com>
2025-12-07 17:50:40 +0100

    feat: disable rocket logging

--- backend/src/api/mod.rs
+++ backend/src/api/mod.rs
@@ -1,22 +1,32 @@
-use rocket::routes;
+use rocket::{Config, routes};
 use rocket_cors::CorsOptions;
 
 mod health;
 mod leaderboard;
 
-trait MountRoutes {
+trait ConfigureRocket {
     fn mount_routes(self: Self) -> Self;
+    fn config(self: Self) -> Self;
 }
 
-impl MountRoutes for rocket::Rocket<rocket::Build> {
+impl ConfigureRocket for rocket::Rocket<rocket::Build> {
     fn mount_routes(self: Self) -> Self {
         self.mount("/", routes![health::health])
             .mount("/leaderboard", routes![leaderboard::index])
+    }
+
+    fn config(self: Self) -> Self {
+        let base = self.figment().clone();
+        self.configure(
+            base.merge((Config::LOG_LEVEL, "off"))
+                .merge((Config::CLI_COLORS, "false")),
+        )
     }
 }
 
 pub fn build() -> rocket::Rocket<rocket::Build> {
     rocket::build()
         .mount_routes()
+        .config()
         .attach(CorsOptions::default().to_cors().unwrap())
 }
